bfa70a8e70329f3e2c02f34d42deb5f9a180e7c6
prakashkukanoor <prakashkop054@gmail.com>
2026-08-22 14:13:28 -0400

    env specific worker node

--- iam.tf
+++ iam.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
 }
 
 resource "aws_iam_role" "eks_node_role" {
-  name = "${var.cluster_name}-node-role"
+  name = "${local.cluster_full_name}-node-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
